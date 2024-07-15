18d89a01d1f9f33799dbef1b2aca7d0b7ac3758f
11535 <1153592594@qq.com>
2024-07-15 19:01:23 +0800

    更新方法的增加

--- service/user_service.py
+++ service/user_service.py
@@ -1,25 +1,42 @@
+import time
+
 from app.crud.crud_user import user_dao
 from db.database import async_db_session
 from schemas.user import *
 from db.models import User
 from util.token import *
+from util.email import dict_captcha
 
 
 class UserService:
 
     @staticmethod  # 用户注册
-    async def create_user(user: CreateUserParam):
+    async def create_user(user: CreateUserParam, captcha):
+        try:
+            now = time.time()
+            if now - dict_captcha[user.email + "time"] > 300:
+                dict_captcha[user.email] = None
+                return False, "验证码超时"
+            if not dict_captcha[user.email] == captcha:
+                return False, "验证码错误"
+        except Exception:
+            return False, "未发送验证码"
+        dict_captcha[user.email] = None
         async with async_db_session.begin() as db:
             if not user.password:
                 raise Exception('Password is required')
             username = await user_dao.get(db=db, name=user.name)
             if username:
                 print("Username already exists")
-                return False
+                return False, "用户名已存在"
+            useremail = await user_dao.get(db=db, email=user.email)
+            if useremail:
+                print("邮箱已被注册")
+                return False, "邮箱已被注册"
             await user_dao.create(db, obj=user)
-        return user
+        return True, user
 
-    @staticmethod
+    @staticmethod  # 查重函数
     async def get_user(id: int | None = None, name: str | None = None):
         async with async_db_session.begin() as db:
             if not id:
@@ -42,6 +59,35 @@ class UserService:
                     return token
                 else:
                     return False
+
+    @staticmethod  # 更新用户密码
+    async def update_password(update_user: UpdateUserParam, captcha):
+        async with async_db_session.begin() as db:
+            user = await user_dao.get(email=update_user.email, db=db)
+            try:
+                now = time.time()
+                user = user[0]
+                if now - dict_captcha[user.email + "time"] > 300:
+                    dict_captcha[user.email] = None
+                    return False, "验证码超时"
+                if not dict_captcha[user.email] == captcha:
+                    return False, "验证码错误"
+            except Exception:
+                return False, "未发送验证码"
+            dict_captcha[user.email] = None
+            id = user.id
+            dict = update_user.dict(exclude_unset=True)
+            await user_dao.update_userinfo(db, obj=dict, id=id)
+            return True, '修改密码成功'
+
+    @staticmethod  # 更新用户所有信息
+    async def update_user(username: str, update_user: UpdateUserParam):
+        async with async_db_session.begin() as db:
+            current_user = await user_dao.get(name=username, db=db)
+            id = current_user[0].id
+            dict = update_user.dict(exclude_unset=True)
+            await user_dao.update_userinfo(db, obj=dict, id=id)
+            return True
 
 
 user_service = UserService()
