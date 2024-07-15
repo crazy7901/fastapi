4a98690be72c08cd673fb065d7fc0dd8c2135d7b
11535 <1153592594@qq.com>
2024-07-15 19:00:16 +0800

    增加球员类

--- db/models.py
+++ db/models.py
@@ -17,9 +17,10 @@ class User(Base):
     password = Column(String(50), comment="密码")
     name = Column(String(50), comment="名字")
     is_active = Column(Boolean, default=True, comment="状态")
-    role = Column(Integer, default=1000,comment="角色 1：队长 2：教练 3：队员,格式为1000，第二位为队长（球队创建者默认为队长），第三位位为教练，第四位为队员")
+    role = Column(Integer, default=1000,
+                  comment="角色 1：队长 2：教练 3：队员,格式为1000，第二位为队长（球队创建者默认为队长），第三位位为教练，第四位为队员")
     club = Column(String(50), index=True, comment="所属球队名", default=None)
-    avatar = Column(Integer, default=0,comment="头像，0-11")
+    avatar = Column(Integer, default=0, comment="头像，0-11")
     # items = relationship("Club", back_populates="player")ateTime, server_default=func.now(), comment="创建时间")  #
     # 使用server_default owner_id=Mapper[]=mapped_column(Integer, index=True, comment="球队创建者")
 
@@ -32,3 +33,13 @@ class Club(Base):
     name = Column(String(50), index=True, comment="俱乐部名")
     avatar = Column(Integer, default=0, comment="头像，0-11")
     # owner = relationship("player", back_populates="Club")
+
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id = Column(Integer, primary_key=True, comment='id')
+    number = Column(Integer, unique=True, index=True, comment="球队号码")
+    club = Column(String(50), comment="俱乐部名")
+    position = Column(String(50), comment="场上位置")
+    name = Column(String(50), comment="球员名")
