fb4dc831cad03ad6746cf53ea65b1bb87a25e3b1
11535 <1153592594@qq.com>
2024-07-17 10:06:34 +0800

    增加了邮箱登录接口

--- app/api/user/api_user.py
+++ app/api/user/api_user.py
@@ -3,22 +3,22 @@ import random
 import re
 import time
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 from fastapi.params import Depends
-from fastapi.security import OAuth2PasswordBearer
+
 from common.response.response_schema import response_base, ResponseModel
 from schemas.club import CreateClubParam
 from schemas.user import *
 from service.club_service import club_service
 from service.user_service import user_service
-from util.token import get_current_token
 from util.email import send_email, dict_captcha
+from util.token import get_current_token
 
 router = APIRouter()
 
 
 # oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-@router.post("/sendEmail/{toaddr}", description="发送邮件验证码")
+@router.post("/sendEmail/{toaddr}", summary="发送邮件验证码")
 async def register(toaddr: str) -> ResponseModel:
     regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     flag = bool(re.match(regex, toaddr))
@@ -34,7 +34,7 @@ async def register(toaddr: str) -> ResponseModel:
         return await response_base.success(data="验证码已发送")
 
 
-@router.post("/register/{captcha}", description="注册接口")
+@router.post("/register/{captcha}", summary="注册接口")
 async def register(user: CreateUserParam, captcha) -> ResponseModel:
     data = await user_service.create_user(user=user, captcha=captcha)
     if data[0]:
@@ -43,20 +43,30 @@ async def register(user: CreateUserParam, captcha) -> ResponseModel:
         return await response_base.fail(data=data[1])
 
 
-@router.post("/login", description="登录接口")
+@router.post("/login", summary="账号登录接口")
 async def login(user: BaseUserParm) -> ResponseModel:
     data = await user_service.check_user(user)
     data = {
         "token": data,
         "username": user.name
     }
-    if data:
+    if data["token"]:
         return await response_base.success(data=data)
     else:
         return await response_base.fail(data="用户名或密码错误")
 
 
-@router.post("/createClub", description="创建俱乐部")
+@router.post("/emaillogin/{captcha}", summary="邮箱登录接口")
+async def emaillogin(user:UpdateUserParam,captcha) -> ResponseModel:
+    email = user.email
+    data = await user_service.check_email(email=email,captcha=captcha)
+    if data[0]:
+        return await response_base.success(data={"token": data[1], "username": data[2]})
+    else:
+        return await response_base.fail(data=data[1])
+
+
+@router.post("/createClub", summary="创建俱乐部")
 async def createClub(club: CreateClubParam, current_user: dict = Depends(get_current_token)) -> ResponseModel:
     data = await club_service.create_club(club, creator=current_user['username'])
     if data[0]:
@@ -70,7 +80,7 @@ async def createClub(club: CreateClubParam, current_user: dict = Depends(get_cur
         return await response_base.fail(data=data[1])
 
 
-@router.put("/reset/{captcha}", description="重置密码接口")
+@router.put("/reset/{captcha}", summary="重置密码接口")
 async def reset(user: UpdateUserParam, captcha) -> ResponseModel:
     data = await user_service.update_password(captcha=captcha, update_user=user)
     if data[0]:
@@ -79,7 +89,7 @@ async def reset(user: UpdateUserParam, captcha) -> ResponseModel:
         return await response_base.fail(data=data[1])
 
 
-@router.put("/update", description="修改用户信息")
+@router.put("/update", summary="修改用户信息")
 async def update(user: UpdateUserParam, current_user: dict = Depends(get_current_token)) -> ResponseModel:
     data = await user_service.update_user(update_user=user, username=current_user['username'])
     if data:
@@ -88,7 +98,7 @@ async def update(user: UpdateUserParam, current_user: dict = Depends(get_current
         return await response_base.fail(data="修改失败")
 
 
-@router.get("/avatar", description="获取头像集")
+@router.get("/avatar", summary="获取头像集")
 async def avatar_list() -> ResponseModel:
     avatars = {}
     for i in range(0, 12):
@@ -99,7 +109,7 @@ async def avatar_list() -> ResponseModel:
     return await response_base.success(data=avatars)
 
 
-@router.put("/updateAvatar", description="更新头像")
+@router.put("/updateAvatar", summary="更新头像")
 async def update_avatar(user: UpdateUserParam, current_user: dict = Depends(get_current_token)) -> ResponseModel:
     data = await user_service.update_user(update_user=user, username=current_user['username'])
     if data:
