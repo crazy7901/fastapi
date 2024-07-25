28cc2e3afe4c3714a311f4b44869fd4ce5e4de4b
11535 <1153592594@qq.com>
2024-07-25 17:58:08 +0800

    更新比赛信息的service筛选部分

--- service/race_service.py
+++ service/race_service.py
@@ -35,5 +35,25 @@ class RaceService:
             result = await race_dao.add_race(db=db, obj=race)
             return result
 
+    @staticmethod  # 队长可以修改比赛时间，
+    async def update_race(username: str, race: UpdateRaceParam):
+        async with async_db_session.begin() as db:
+            role = await user_dao.get(db=db,name=username)
+            if not race.id:
+                return False, "比赛 ID 不能为空"
+            if role[0].role // 1000 == 2:  # 2开头的为管理员,有所有权限
+                dict = race.dict(exclude_unset=True)
+                result = await race_dao.update_race(db=db, obj=dict, id=race.id)
+                return result,''
+            if role[0].role // 100 == 11:  # 2开头的为管理员
+                if race.homeTeamGoalsScored or race.awayTeamGoalsScored:
+                    return False, "队长不能修改比分"
+                dict = race.dict(exclude_unset=True)
+                result = await race_dao.update_race(db=db, obj=dict,id=race.id)
+                return result,''
+            else:
+                return False, "权限不足"
+
+
 
 race_service = RaceService()
